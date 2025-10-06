85001feced1f7b4bcd07215e1be33ce0d8784d4d
DonovanKen <donovankengiseh@gmail.com>
2025-10-06 00:27:06 -0400

    adding provider

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  # Auth via variables (valeurs injectées par Jenkins en TF_VAR_*)
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+}
